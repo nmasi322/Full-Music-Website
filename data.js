4e53ff6955a6048b34ebc8304b21232a2848db9c
nmasi322 <edehdivine042@gmail.com>
2021-09-28 09:43:03 +0100

    added html files

---
+++ data.js
@@ -0,0 +1,92 @@
+let songs = [
+    {
+        name: "Cabin Fever",
+        path: "./audio/cabin.mp3",
+        artist: "Jaden Smith",
+        cover: "./images/jaden.jpg"
+    },
+    { 
+        name: "Cast",
+        path: "./audio/cast.mp3",
+        artist: "Ayra Starr",
+        cover: "./images/ayra.jpg"
+    },
+    {
+        name: "Dangerous",
+        path: "./audio/dangerous.mp3",
+        artist: "Cheque & Ayra Starr",
+        cover: "./images/bravo.jpg"
+    },
+    {
+        name: "Emotions",
+        path: "./audio/emotions.mp3",
+        artist: "Peni",
+        cover: "./images/emotions.jpg"
+    },
+    {
+        name: "Everything Sucks",
+        path: "./audio/everything-sucks.mp3",
+        artist: "Vault Boy",
+        cover: "./images/everything-sucks.jpg"
+    },
+    {
+        name: "Falling For You",
+        path: "./audio/falling.mp3",
+        artist: "Jaden Smith & Justin Bieber",
+        cover: "./images/jaden.jpg"
+    },
+    {
+        name: "Felony",
+        path: "./audio/felony.mp3",
+        artist: "Ckay",
+        cover: "./images/felony.jpg"
+    },
+    {
+        name: "High",
+        path: "./audio/high.mp3",
+        artist: "Adekunle Gold & Davido",
+        cover: "./images/high.jpg"
+    },
+    {
+        name: "History",
+        path: "./audio/history.mp3",
+        artist: "Cheque & Fireboy",
+        cover: "./images/fireboy.jpg"
+    },
+    {
+        name: "Karma",
+        path: "./audio/karma.mp3",
+        artist: "Ayra Starr",
+        cover: "./images/ayra.jpg"
+    },
+    {
+        name: "LOML",
+        path: "./audio/loml.mp3",
+        artist: "Cheque & Olamide",
+        cover: "./images/bravo.jpg"
+    },
+    {
+        name: "Mystery Girl",
+        path: "./audio/mystery-girl.mp3",
+        artist: "Johny Drille",
+        cover: "./images/mystery-girl.jpg"
+    },
+    {
+        name: "Sinner",
+        path: "./audio/sinner.mp3",
+        artist: "Adekunle Gold & Lucky Daye",
+        cover: "./images/sinner.jpg"
+    },
+    {
+        name: "Snitch",
+        path: "./audio/snitch.mp3",
+        artist: "Ayra Starr & Foushee",
+        cover: "./images/ayra.jpg"
+    },
+    {
+        name: "Understand",
+        path: "./audio/understand.mp3",
+        artist: "Omah Lay",
+        cover: "./images/understand.jpg"
+    }
+]

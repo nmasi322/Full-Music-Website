158ef25ca5f027d98095f234c0a37e184a372e55
nmasi322 <edehdivine042@gmail.com>
2021-09-30 01:39:32 +0100

    Added light theme option

--- app.js
+++ app.js
@@ -241,3 +241,19 @@ queue.forEach((item, i) => {
 //         playBtn.click();
 //     })
 // })
+
+
+// light theme
+
+let icon = document.getElementById('icon');
+
+icon.onclick = function(){
+    document.body.classList.toggle('light-theme');
+
+    if (document.body.classList.contains('light-theme')){
+        icon.src = "./images/sun.png";
+    }
+    else{
+        icon.src = "./images/moon.png"
+    }
+}
